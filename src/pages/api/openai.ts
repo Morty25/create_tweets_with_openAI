3682cce43fce3a52cf2f3b659163f1eed7fc4eed
Alain <Waralain25555@hotmail.Fr>
2023-02-24 13:14:28 +0100

    modal settings

--- src/pages/api/openai.ts
+++ src/pages/api/openai.ts
@@ -1,28 +1,32 @@
 import axios from "axios";
 
-const getTweets = async (article: string, caratereMax: number) => {
-    const commande = `Peut tu me créer un JSON avec trois tweets différents grace à l'article suivant ?`;
-    return await axios({
-      method: "POST",
-      url: "https://api.openai.com/v1/completions",
-      data: {
-        model: "text-davinci-003",
-        prompt: `${commande} Article: ${article}`,
-        temperature: 0.5,
-        max_tokens: caratereMax * 3,
-        top_p: 1.0,
-        frequency_penalty: 0.8,
-        presence_penalty: 0.0,
-      },
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: process.env.OPENAI_API_KEY,
-      },
-    })
+const getTweets = async (
+  article: string,
+  caratereMax: number,
+  temperature: number,
+  presence_penalty: number,
+  frequency_penalty: number
+) => {
+  const commande = `Peut tu me créer un JSON avec trois tweets différents grace à l'article suivant ? (Ne pas numéroter les tweets, un tweet doit avoir un ou deux hastag, un tweet ne dois pas dépasser ${caratereMax} caractères)`;
+  return await axios({
+    method: "POST",
+    url: "https://api.openai.com/v1/completions",
+    data: {
+      model: "text-davinci-003",
+      prompt: `${commande} Article: ${article}`,
+      temperature: temperature,
+      max_tokens: caratereMax * 3,
+      top_p: 1.0,
+      frequency_penalty: frequency_penalty,
+      presence_penalty: presence_penalty,
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: process.env.OPENAI_API_KEY,
+    },
+  });
 };
 
 export const APIOpenAI = {
-    getTweets: getTweets,
+  getTweets: getTweets,
 };
-
-// de ${caratereMax} caractère maximum
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,140 @@
 import Head from "next/head";
-import { Button, Card, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+import SettingsSuggestOutlinedIcon from "@mui/icons-material/SettingsSuggestOutlined";
+import {
+  Button,
+  Card,
+  Stack,
+  TextField,
+  Typography,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  Slider,
+} from "@mui/material";
 import Textarea from "@mui/joy/Textarea";
 import { useTheme } from "@mui/material/styles";
-import { useState } from "react";
 
 import { APIOpenAI } from "./api/openai";
 
-/* DOC */
-// prompt: Le texte saisi.
 // engine: OpenAI a mis à disposition quatre moteurs de complétion de texte, nommés davinci, adaet . Nous utilisons , qui est le plus capable des quatre, mais le plus lent et le plus cher si vos besoins vont au-delà du niveau gratuit disponible pour l'API.babbagecuriedavinci
 // stop: Le moteur GPT-3 ne "comprend" pas vraiment le texte, donc lorsqu'il génère du texte, il a besoin de savoir quand s'arrêter. Dans l'exemple de la construction d'un chat bot, en donnant un arrêt de "Humain :", nous disons au moteur de générer simplement du texte pour la ligne qui commence par "Bot :". Sans marqueur d'arrêt, GPT-3 continuerait à générer du texte en écrivant plus de lignes pour l'utilisateur et l'IA.
-// temperature: nombre compris entre 0 et 1 qui détermine le nombre de risques créatifs pris par le moteur lors de la génération de texte.
 // top_p: Une manière alternative de contrôler l'originalité et la créativité du texte généré.
-// frequency_penalty: Un nombre compris entre 0 et 1. Plus cette valeur est élevée, plus le modèle fera d'efforts pour ne pas se répéter.
-// presence_penalty: Un nombre compris entre 0 et 1. Plus cette valeur est élevée, plus le modèle fait d'efforts pour parler de nouveaux sujets.
-// max_tokens: Longueur de complétion maximale.
-/* FIN DOC */
+interface IModalSettings {
+  state: boolean;
+  setState: (state: boolean) => void;
+  temperature: number;
+  setTemperature: (temperature: number) => void;
+  presence_penalty: number;
+  setPresence_penalty: (presence_penalty: number) => void;
+  frequency_penalty: number;
+  setFrequency_penalty: (frequency_penalty: number) => void;
+}
 
+const ModalSettings = (props: IModalSettings) => {
+  const {
+    state,
+    setState,
+    temperature,
+    setTemperature,
+    presence_penalty,
+    setPresence_penalty,
+    frequency_penalty,
+    setFrequency_penalty,
+  } = props;
+
+  const marks = [0, 0.2, 0.4, 0.6, 0.8, 1].map((e) => ({
+    value: e,
+    label: `${e}`,
+  }));
+
+  return (
+    <Dialog
+      open={state}
+      onClose={() => setState(false)}
+      //   PaperComponent={PaperComponent}
+      aria-labelledby="draggable-dialog-title"
+    >
+      <DialogTitle style={{ cursor: "move" }}>Paramétre</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Temperature : {temperature} <br />
+          Presence_penalty : {presence_penalty} <br />
+          Frequency_penalty : {frequency_penalty}
+        </DialogContentText>
+      </DialogContent>
+      <DialogContent>
+        <DialogContentText>
+          temperature : nombre compris entre 0 et 1 qui détermine le nombre de
+          risques créatifs pris par le moteur lors de la génération de texte.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ width: "80%", marginLeft: "10%" }}>
+        <Slider
+          aria-label="Temperature"
+          valueLabelDisplay="auto"
+          marks={marks}
+          value={temperature}
+          onChange={(e) => e.target && setTemperature(Number(e.target.value))}
+          step={0.1}
+          min={0}
+          max={1}
+        />
+      </DialogActions>
+
+      <DialogContent>
+        <DialogContentText>
+          presence_penalty: Un nombre compris entre 0 et 1. Plus cette valeur
+          est élevée, plus le modèle fait d'efforts pour parler de nouveaux
+          sujets.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ width: "80%", marginLeft: "10%" }}>
+        <Slider
+          aria-label="Presence_penalty"
+          valueLabelDisplay="auto"
+          marks={marks}
+          value={presence_penalty}
+          onChange={(e) =>
+            e.target && setPresence_penalty(Number(e.target.value))
+          }
+          step={0.1}
+          min={0}
+          max={1}
+        />
+      </DialogActions>
+
+      <DialogContent>
+        <DialogContentText>
+          frequency_penalty: Un nombre compris entre 0 et 1. Plus cette valeur
+          est élevée, plus le modèle fera d'efforts pour ne pas se répéter.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ width: "80%", marginLeft: "10%" }}>
+        <Slider
+          aria-label="Frequency_penalty"
+          valueLabelDisplay="auto"
+          marks={marks}
+          value={frequency_penalty}
+          onChange={(e) =>
+            e.target && setFrequency_penalty(Number(e.target.value))
+          }
+          step={0.1}
+          min={0}
+          max={1}
+        />
+      </DialogActions>
+
+      <DialogActions>
+        <Button onClick={() => setState(false)}>Fermer</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 interface ICardTweet {
   tweet: string;
   url: string;
@@ -33,6 +151,7 @@ const CardTweet = (props: ICardTweet) => {
     : `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweet)}`;
   return (
     <Card
+      variant="outlined"
       sx={{
         maxWidth: "400px",
         minWidth: "300px",
@@ -40,7 +159,7 @@ const CardTweet = (props: ICardTweet) => {
         margin: theme.spacing(2),
       }}
     >
-      <Stack flexDirection="column" spacing={1}>
+      <Stack spacing={1}>
         <Stack>{tweet}</Stack>
         <Stack>{url}</Stack>
         <a href={urlTwiter} target="_blank" rel="noreferrer">
@@ -59,22 +178,30 @@ const CardTweet = (props: ICardTweet) => {
 
 const App = () => {
   const theme = useTheme();
-  const [loading, setLoading] = useState(false);
-  const [tweets, setTweets] = useState([]);
-  const [urlLie, setUrlLie] = useState("");
-  const [article, setArticle] = useState("");
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [temperature, setTemperature] = useState<number>(0.5);
+  const [frequency_penalty, setFrequency_penalty] = useState<number>(0.8);
+  const [presence_penalty, setPresence_penalty] = useState<number>(0);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [tweets, setTweets] = useState<string[]>([]);
+  const [urlLie, setUrlLie] = useState<string>("");
+  const [article, setArticle] = useState<string>("");
   const caratereMax = 280 - urlLie.length;
 
   const getTweet = () => {
     if (article) {
       setLoading(true);
-      APIOpenAI.getTweets(article, caratereMax)
+      APIOpenAI.getTweets(article, caratereMax, temperature, presence_penalty, frequency_penalty)
         .then((res: any) => {
           if (res.status === 200) {
             console.log(res);
             const jsonTweet = JSON.parse(res.data.choices[0].text);
-            console.log(jsonTweet);
-            setTweets(jsonTweet.map((j: any) => j.tweet));
+            if (jsonTweet[0].tweet) {
+              setTweets(jsonTweet.map((j: any) => j.tweet));
+            } else {
+              setTweets(jsonTweet.map((j: any) => j.text));
+            }
             setLoading(false);
           }
         })
@@ -94,8 +221,22 @@ const App = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Stack alignItems="center" width="100%" spacing={4}>
-        <Stack padding={theme.spacing(4)} sx={{ textAlign: "center" }}>
-          Création de Tweet via Open AI. (GPT3)
+        <Stack
+          padding={theme.spacing(4)}
+          justifyContent="space-between"
+          alignItems="center"
+          direction="row"
+          width="100%"
+        >
+          <Stack width="24px" />
+          <Typography>Création de Tweet via Open AI. (GPT3)</Typography>
+          <IconButton
+            color="primary"
+            aria-label="settings"
+            onClick={() => setOpenModal(true)}
+          >
+            <SettingsSuggestOutlinedIcon />
+          </IconButton>
         </Stack>
 
         <Stack
@@ -150,6 +291,17 @@ const App = () => {
             </Stack>
           )}
         </Stack>
+
+        <ModalSettings
+          state={openModal}
+          setState={setOpenModal}
+          temperature={temperature}
+          setTemperature={setTemperature}
+          presence_penalty={presence_penalty}
+          setPresence_penalty={setPresence_penalty}
+          frequency_penalty={frequency_penalty}
+          setFrequency_penalty={setFrequency_penalty}
+        />
       </Stack>
     </>
   );
